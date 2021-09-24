2f2a3b7a56799491d7dbc6681180eba7c323de55
Aaron Gabriel <aaronlwgabriel@outlook.com>
2021-09-23 23:45:40 -0500

    update selector root naming

--- main/apps/shell/src/app/home/home.component.ts
+++ main/apps/shell/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'aaron-home',
+  selector: 'mode-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
