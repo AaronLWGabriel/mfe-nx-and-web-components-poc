5d34f6ab2fb7a41144e05e0b27c7e6dbfbe2bd42
Aaron Gabriel <aaron.gabriel@gmfinancial.com>
2021-09-29 13:48:17 -0500

    stage changes

--- main/apps/shell/src/app/app.component.ts
+++ main/apps/shell/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare const require: any;
 
 @Component({
   selector: 'mode-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shell';
+  public readonly title = 'shell';
+
+  ngVersion = require('../../../../package.json').dependencies['@angular/core'];
 }
--- marketing/src/app/app.component.ts
+++ marketing/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+declare const require: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'marketing';
+  ngVersion = require('../../package.json').dependencies['@angular/core'];
 }
